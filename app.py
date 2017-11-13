252d4afd8039a5f8fe6a2f8fa409778fa74c681b
Souheil CHELFOUH <trollfot@gmail.com>
2017-11-13 15:21:24 +0100

    Using now a portal registry

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ with Configuration('etc/config.json') as config:
 
     from fanstatic import Fanstatic
     from rutter.urlmap import URLMap
-    from uvc.concierge import wrapper
+    from uvc.concierge import wrapper, PORTALS_REGISTRY
     from uvc.concierge.portals import XMLRPCPortal
     from uvc.concierge.websockets.app import handle
     from uvc.concierge.websockets.websocket import WebSocketWSGI
     from uvc.concierge.login import logout_app, login_center
+    from uvc.concierge.ticket import cipher 
     from wsgicors import CORS
     from wsgiproxy import HostProxy
     from repoze.who.config import make_middleware_with_config
@@ -25,25 +26,37 @@ with Configuration('etc/config.json') as config:
     plone.login_url = "http://karl.novareto.de:7080/Plone/"
     plone.string_keys = ['REMOTE_USER',]
     plone.use_x_headers = True
+    plone.login_method = 'xmlrpc'
 
     href = 'http://karl.novareto.de:8090/app/++vh++http:karl.novareto.de:8000/uvcsite/++'
     uvcsite = HostProxy(href, client="urllib3")
     uvcsite.login_url = "http://karl.novareto.de:8090/app/"
     uvcsite.string_keys = ['REMOTE_USER',]
     uvcsite.use_x_headers = True
-    
+    uvcsite.login_method = 'json'
+
+    # Registry
+    PORTALS_REGISTRY['plone'] = plone
+    PORTALS_REGISTRY['uvcsite'] = uvcsite
+
     # The application.
     mapping = URLMap()
     mapping['/plone'] = wrapper(mapping, plone)
     mapping['/uvcsite'] = wrapper(mapping, uvcsite)
     mapping['/socket'] = WebSocketWSGI(mapping, handle)
     mapping['/socket/'] = WebSocketWSGI(mapping, handle)
-    mapping['/login'] = login_center(mapping)
+    mapping['/login'] = login_center
     mapping['/logout'] = logout_app
+
+    #AES Cipher
+    application = cipher(
+        mapping, {},
+        cipher_key="BLABBLUB12345678"
+    )
 
     # Middlewares wrapping
     application = make_middleware_with_config(
-        mapping, {'here': path.dirname(__file__)},
+        application, {'here': path.dirname(__file__)},
         config_file=config['who']['config'],
         log_file="stdout",
         log_level="info",
